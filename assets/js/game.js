0f8dd563fcc58b097982288bdee5ed5b124e15e7
Alyssa Petersen <alyssapetersen@Alyssas-MacBook-Pro.local>
2022-07-22 19:06:07 -0400

    initial console log set up

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! JavaScript is running!")
+// this asks for the robots name
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//What is this?
+console.log("Our robot's name is " + playerName)
+
+// this creates a function names "fight"
+function fight() {
+    console.log("The fight has begun!");
+}
+
+
+
+
+
+
+
+//fight();
